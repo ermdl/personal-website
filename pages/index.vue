70434bcacc83608aae411eec4408a85122179fa6
Danila Ermolenko <danila@fluco.studio>
2024-08-26 10:54:01 +0100

    Added icons, gsap and random images for tests

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,15 @@
 <template>
-  <div class="text-4xl">Home page</div>
-  <NuxtImg src="https://picsum.photos/1000" alt="" />
+  <div>
+    <div class="text-4xl">Home page</div>
+    <div class="grid grid-cols-3 gap-4">
+      <NuxtImg v-for="image in 100" :src="getRandomImage(image)" alt="" />
+    </div>
+    <NuxtLink to="/about"> </NuxtLink>
+  </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const getRandomImage = (index: number) => {
+  return `https://picsum.photos/1000?random=${index}`
+}
+</script>
